d763ab39426e2517f6c814843489bfbe96794d7d
Andy Puertas <andy.puertas08@gmail.com>
2018-05-11 14:54:32 -0600

    70% done

--- src/App.js
+++ src/App.js
@@ -3,16 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      pictureInput: '',
+      nameInput: '',
+      listOfFriends: []
+    }
+  }
+
+  pictureChange(val){
+    this.setState({ pictureInput: val })
+  }
+
+  nameChange(val){
+    this.setState({ nameInput: val })
+  }
+
+  handleClick(){
+    let newFriend = {
+      url: this.state.pictureInput,
+      name: this.state.nameInput
+    }
+    this.setState({
+      listOfFriends: [...this.state.listOfFriends, newFriend],
+      pictureInput: '',
+      nameInput: ''
+    })
+  }
+
   render() {
+    let list = this.state.listOfFriends.map( (friends, index) => {
+      return (
+        <div key={index}>
+          <p>Name: {friends.name}</p>
+        </div>
+      )
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Picture:</p>
+        <input 
+        value={this.state.pictureInput} 
+        onChange={(e) => this.pictureInput(e.target.value)} />
+        <p>Name:</p>
+        <input 
+         value={this.state.nameInput} 
+          onChange={(e) => this.nameInput(e.target.value)} />
+        <br /> 
+        <button onClick={() => this.handleClick()}>Add Friend</button>
       </div>
     );
   }
